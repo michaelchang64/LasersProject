1b8ed2b0c654b0fa6b3ecd12f313d8241c10fcb9
michaelchang <michael.y.chang@yale.edu>
2018-01-31 10:44:46 -0500

    changed code for twitterbot so it uses WiFi shield library instead. Have yet to test on laser

--- TwitterBot/twitterbot/twitterbot.ino
+++ TwitterBot/twitterbot/twitterbot.ino
@@ -1,23 +1,39 @@
 //twitter token: 2413286622-TKO6va6Z3Q66X96W570BhibHE6U2mfL25TZHlJA
-#if defined(ARDUINO) && ARDUINO > 18   // Arduino 0019 or later
+//#if defined(ARDUINO) && ARDUINO > 18   // Arduino 0019 or later
 #include <SPI.h>
 #endif
-#include <Ethernet.h>
-//#include <EthernetDNS.h>  Only needed in Arduino 0022 or earlier
+#include <WiFi.h>
 #include <Twitter.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 10, 0, 0, 177 };
+//byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+char ssid[] = "YaleGuest";
+int status = WL_IDLE_STATUS;
 Twitter twitter("2413286622-TKO6va6Z3Q66X96W570BhibHE6U2mfL25TZHlJA");
-char msg[] = "Hello, World! I'm Arduino!";
+char msg[] = "Hello, world!";
 
-void setup()
-{
-  delay(1000);
-  Ethernet.begin(mac, ip);
-  Serial.begin(9600);
+void setup() {
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // don't continue:
+    while(true);
+  } 
   
-  Serial.println("connecting...");
+ // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to open SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+    
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+  
   if (twitter.post(msg)) {
     int status = twitter.wait();
     if (status == 200) {
@@ -31,6 +47,74 @@ void setup()
   }
 }
 
-void loop()
-{
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  printCurrentNet();
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+  
+  // print your MAC address:
+  byte mac[6];  
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+  
+  // print your subnet mask:
+  IPAddress subnet = WiFi.subnetMask();
+  Serial.print("NetMask: ");
+  Serial.println(subnet);
+
+  // print your gateway address:
+  IPAddress gateway = WiFi.gatewayIP();
+  Serial.print("Gateway: ");
+  Serial.println(gateway);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);    
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5],HEX);
+  Serial.print(":");
+  Serial.print(bssid[4],HEX);
+  Serial.print(":");
+  Serial.print(bssid[3],HEX);
+  Serial.print(":");
+  Serial.print(bssid[2],HEX);
+  Serial.print(":");
+  Serial.print(bssid[1],HEX);
+  Serial.print(":");
+  Serial.println(bssid[0],HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption,HEX);
 }
